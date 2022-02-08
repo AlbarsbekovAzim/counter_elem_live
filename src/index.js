7a0142a64c81b040f89f926c88e3b2bdbc18be8a
AlbarsbekovAzim <albarsbekovazim@gmail.com>
2022-02-08 23:55:45 +0600

    done elements live

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+class Counter extends React.Component {
+  state = {
+    count: 0
+  }
+  onIncrease = () => {
+    this.setState((oldState) => {
+      return {
+        count: oldState + 1
+      }
+    })
+  }
+  onDecrease = () => {
+    this.setState((oldState) => {
+      return {
+        count: oldState - 1
+      }
+    })
+  }
+  UNSAFE_componentWillMount = () => {
+    console.log('WILL MOUNT')
+  }
+  componentDidMount = () => {
+    this.interval = setInterval(() => {
+      console.log('Hello Kubanych')
+    }, 2000)
+    console.log('DID MOUNT')
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.count != this.state.count) {
+      return true
+    } else {
+      return false
+    }
+  }
+  UNSAFE_componentWillUpdate = () => {
+    console.log('WILL UPDATE')
+  }
+  componentDidUpdate = () => {
+    console.log('DID UPDATE')
+  }
+  componentWillUnmount = () => {
+    clearInterval(this.interval)
+    console.log('WILL UNMOUNT')
+  }
+  render() {
+    console.log('RENDER')
+    return (
+        <div>
+          <p>{this.state.count}</p>
+          <input onClick={this.onIncrease} type='button' value='Increase'/>
+          <input onClick={this.onDecrease} type='button' value='Decrease'/>
+        </div>
+    );
+  };
+};
+class App extends React.Component {
+  state = {
+    showCounter: true
+  }
+  onToggleCounter = () => {
+    this.setState((oldState) => {
+      return {
+        showCounter: !oldState.showCounter
+      }
+    })
+  }
+  render() {
+    content = this.state.showCounter ? <Counter /> : null
+    return (
+      <>
+        {content}
+        <button onClick={this.onToggleCounter}>Toggle counter</button>
+      </>
+    );
+  }
+};
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
